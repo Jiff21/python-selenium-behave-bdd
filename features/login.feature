2d685ac2508795aef08580ce62203d9bbbe07568
Jeff Campbell <3317888+Jiff21@users.noreply.github.com>
2023-09-30 07:43:33 -0700

    renamed index.feature, login.feature addded a first name validation test

--- features/login.feature
+++ features/login.feature
@@ -10,3 +10,13 @@ Feature: Tests that should be run on every page
       And I click on the Login Button
       And I wait for the Menu button
     Then the url should contain "/v1/inventory.html"
+
+  @browser  @critical
+  Scenario: Validation error when submitting without Username
+    Given I am on "index"
+    When I click on the Password field
+      And I type in "secret_sauce"
+      And I click on the Login Button
+      And I wait for the Menu button
+    Then there should be one validation error
+
--- features/steps/login.py
+++ features/steps/login.py
@@ -12,6 +12,7 @@ from settings import HOST_URL, PAGES_DICT
 USERNAME_LOCATOR = (By.ID, 'user-name')
 PASSWORD_LOCATOR = (By.ID, 'password')
 LOGIN_BUTTON = (By.ID, 'login-button')
+VALIDATIOR_ERROR = (By.CSS_SELECTOR, '.error-button')
 
 
 @step('I click on the Username field')
@@ -20,14 +21,32 @@ def step_impl(context):
     context.current_element = wait.until(EC.visibility_of_element_located(USERNAME_LOCATOR))
     context.current_element.click()
 
+
 @step('I click on the Password field')
 def step_impl(context):
     wait = WebDriverWait(context.driver, 10)
     context.current_element = wait.until(EC.visibility_of_element_located(PASSWORD_LOCATOR))
     context.current_element.click()
 
+
 @step('I click on the Login Button')
 def step_impl(context):
     wait = WebDriverWait(context.driver, 10)
     context.current_element = wait.until(EC.visibility_of_element_located(LOGIN_BUTTON))
     context.current_element.click()
+
+
+@step('there should be one validation error')
+def step_impl(context):
+    error_messages = context.driver.find_elements(*VALIDATIOR_ERROR)
+    assert len(error_messages) > 0 and len(error_messages) < 1, 'Expected 1 validation error' \
+        ', found {} error elements'.format(len(error_messages))
+
+
+@step('the validation error should include "{string}"')
+def step_impl(context, string):
+    context.current_element = context.driver.find_element(*VALIDATIOR_ERROR)
+    assert string in context.current_element.text, 'Expected "{expected}" to but in "{result}"'.format(
+        expected=string,
+        result=context.current_element.text
+    )
